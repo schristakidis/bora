6685ae235b29e9809eb0386440dc1501647fb34c
loox <loox@iamnothere.org>
2014-05-30 19:10:35 +0300

    gotcookie functions

--- cookie_sender.c
+++ cookie_sender.c
@@ -19,6 +19,7 @@ static SendData bogusData;
 static SendData ckData[2];
 static CookieAck ckAck[2];
 static int havecookie[2];
+static int gotcookie[2];
 //static pthread_mutex_t ckLock = PTHREAD_MUTEX_INITIALIZER;
 
 int send_cookie (struct sockaddr_in * addr1, struct sockaddr_in * addr2) {
@@ -30,6 +31,8 @@ int send_cookie (struct sockaddr_in * addr1, struct sockaddr_in * addr2) {
     memcpy(&ckData[1].to, addr2, sizeof(struct sockaddr_in));
     havecookie[0] = 1;
     havecookie[1] = 1;
+    gotcookie[0] = 1;
+    gotcookie[1] = 1;
     //puts("setting fields");
     //printf("field1: %i field2: %i\n", havecookie[0], havecookie[1]);
     sem_post(&ckFull);
@@ -40,7 +43,7 @@ int send_cookie (struct sockaddr_in * addr1, struct sockaddr_in * addr2) {
 
 void check_answers (void)
 {
-    if (havecookie[1]==0 && havecookie[0] == 0) {
+    if (gotcookie[1]==0 && gotcookie[0] == 0) {
         memcpy(&ckAck, &ckResult, sizeof(CookieAck[2]));
         sem_post(&ckEmpty);
     }
@@ -69,8 +72,8 @@ SendData * get_cookie_data (void) {
 int cookie_received (AckStore * ack) {
     //pthread_mutex_lock(&ckLock);
 
-    if (havecookie[0] == 1 && memcmp(ack->addr, &ckData[0].to, sizeof(struct sockaddr_in)) == 0) {
-        havecookie[0] = 0;
+    if (gotcookie[1] == 0 && memcmp(ack->addr, &ckData[0].to, sizeof(struct sockaddr_in)) == 0) {
+        gotcookie[0] = 1;
         memcpy(&ckAck[0].addr, ack->addr, sizeof(struct sockaddr_in));
         memcpy(&ckAck[0].sent, &ack->sent, sizeof(struct timeval));
         memcpy(&ckAck[0].RTT, &ack->RTT, sizeof(struct timeval));
@@ -82,8 +85,8 @@ int cookie_received (AckStore * ack) {
         return 1;
     }
 
-    if (havecookie[1] == 1 && memcmp(ack->addr, &ckData[1].to, sizeof(struct sockaddr_in)) == 0) {
-        havecookie[1] = 0;
+    if (gotcookie[1] == 1 && memcmp(ack->addr, &ckData[1].to, sizeof(struct sockaddr_in)) == 0) {
+        gotcookie[1] = 1;
         memcpy(&ckAck[1].addr, ack->addr, sizeof(struct sockaddr_in));
         memcpy(&ckAck[1].sent, &ack->sent, sizeof(struct timeval));
         memcpy(&ckAck[1].RTT, &ack->RTT, sizeof(struct timeval));
@@ -95,9 +98,9 @@ int cookie_received (AckStore * ack) {
         return 1;
     }
     //pthread_mutex_unlock(&ckLock);
-    //printf("\nACK: %s:%i ", inet_ntoa(((struct sockaddr_in *)ack->addr)->sin_addr), ((struct sockaddr_in *)ack->addr)->sin_port);
-    //printf("C1: %s:%i ", inet_ntoa((ckData[0].to).sin_addr), (ckData[0].to).sin_port);
-    //printf("C2: %s:%i ", inet_ntoa(ckData[1].to.sin_addr), ckData[1].to.sin_port);
+    printf("\nACK: %s:%i ", inet_ntoa(((struct sockaddr_in *)ack->addr)->sin_addr), ((struct sockaddr_in *)ack->addr)->sin_port);
+    printf("C1: %s:%i ", inet_ntoa((ckData[0].to).sin_addr), (ckData[0].to).sin_port);
+    printf("C2: %s:%i ", inet_ntoa(ckData[1].to.sin_addr), ckData[1].to.sin_port);
     puts("BAD COOKIE RECEIVED!");
     return 0;
 }

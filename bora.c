a50747ad1a6005d8fec20b5ab5e26ff7140e2989
loox <loox@iamnothere.org>
2014-06-23 20:21:40 +0300

    test win_32_aton

--- bora.c
+++ bora.c
@@ -61,6 +61,102 @@ int init_wsa (void) {
 	return 1;
 }
 
+int
+win32_inet_aton(const char *cp, struct in_addr *addr)
+{
+    register unsigned int val;
+    register int base, n;
+    register char c;
+    unsigned int parts[4];
+    register unsigned int *pp = parts;
+
+    assert(sizeof(val) == 4);
+
+    c = *cp;
+    while(1) {
+        /*
+         * Collect number up to ``.''.
+         * Values are specified as for C:
+         * 0x=hex, 0=octal, isdigit=decimal.
+         */
+        if(!isdigit(c))
+            return (0);
+        val = 0; base = 10;
+        if(c == '0') {
+            c = *++cp;
+            if(c == 'x' || c == 'X')
+                base = 16, c = *++cp;
+            else
+                base = 8;
+        }
+        while(1) {
+            if(isascii(c) && isdigit(c)) {
+                val = (val * base) + (c - '0');
+                c = *++cp;
+            } else if(base == 16 && isascii(c) && isxdigit(c)) {
+                val = (val << 4) |
+                    (c + 10 - (islower(c) ? 'a' : 'A'));
+                c = *++cp;
+            } else
+                break;
+        }
+        if(c == '.') {
+            /*
+             * Internet format:
+             *    a.b.c.d
+             *    a.b.c    (with c treated as 16 bits)
+             *    a.b    (with b treated as 24 bits)
+             */
+            if(pp >= parts + 3)
+                return (0);
+            *pp++ = val;
+            c = *++cp;
+        } else
+            break;
+    }
+    /*
+     * Check for trailing characters.
+     */
+    if(c != '\0' && (!isascii(c) || !isspace(c)))
+        return (0);
+    /*
+     * Concoct the address according to
+     * the number of parts specified.
+     */
+    n = pp - parts + 1;
+    switch(n) {
+
+    case 0:
+        return (0);        /* initial nondigit */
+
+    case 1:                /* a -- 32 bits */
+        break;
+
+    case 2:                /* a.b -- 8.24 bits */
+        if((val > 0xffffff) || (parts[0] > 0xff))
+            return (0);
+        val |= parts[0] << 24;
+        break;
+
+    case 3:                /* a.b.c -- 8.8.16 bits */
+        if((val > 0xffff) || (parts[0] > 0xff) || (parts[1] > 0xff))
+            return (0);
+        val |= (parts[0] << 24) | (parts[1] << 16);
+        break;
+
+    case 4:                /* a.b.c.d -- 8.8.8.8 bits */
+        if((val > 0xff) || (parts[0] > 0xff) ||
+           (parts[1] > 0xff) || (parts[2] > 0xff))
+            return (0);
+        val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
+        break;
+    }
+    if(addr)
+        addr->s_addr = htonl(val);
+    return (1);
+}
+
+
 #endif
 
 typedef struct {
@@ -622,22 +718,30 @@ static PyObject *send_block( PyObject * self, PyObject * args )
     destaddr.sin_family = AF_INET;
 
     #if defined(_WIN32) || defined(_WIN64)
-    destaddr.sin_addr.s_addr = s = inet_addr((const char*)dest);
+    s = win32_inet_aton((const char*)dest, &(destaddr.sin_addr.s_addr));
+
+    if (s == 0) {
+                  PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, got: %s", s, PyString_AsString(PyDict_GetItemString(dict_cur, "ip")));
+                  return NULL;
+    }
     #else
     s = inet_pton(AF_INET, (const char*)dest, &(destaddr.sin_addr));
-    #endif
-/*
+
     if (s <= 0) {
                if (s == 0) {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, got: %s", s, PyString_AsString(PyDict_GetItemString(dict_cur, "ip")));
                   return NULL;
                } else {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, got: %s", s, PyString_AsString(PyDict_GetItemString(dict_cur, "ip")));
                   perror("inet_pton");
                   return NULL;
                }
     }
-*/
+    #endif
+
     if (port_num>65535 || port_num<1) {
                   PyErr_SetString(PyExc_AttributeError, "Port number not allowed");
                   return NULL;
@@ -686,26 +790,33 @@ static PyObject *send_raw( PyObject * self, PyObject * args )
 
     memcpy(&message.data[1], message_string, message.length);
     message.length = message.length+1;
-    
+
     #if defined(_WIN32) || defined(_WIN64)
-    message.to.sin_addr.s_addr = s = inet_addr((const char*)dest);
+    s = win32_inet_aton((const char*)dest, &(message.to.sin_addr));
+
+    if (s == 0) {
+                  PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, got: %s", s, dest);
+                  return NULL;
+    }
+
     #else
     s = inet_pton(AF_INET, (const char*)dest, &(message.to.sin_addr));
-    #endif
-/*
     if (s <= 0) {
                if (s == 0) {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
-                  printf("0: %s", dest);
+                  printf("return: %i, got: %s", s, dest);
                   return NULL;
                } else {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, got: %s", s, dest);
                   perror("inet_pton");
-                  printf("%i: %s", dest);
                   return NULL;
                }
     }
-*/
+
+    #endif
+
     if (port_num>65535 || port_num<1) {
                   PyErr_SetString(PyExc_AttributeError, "Port number not allowed");
                   return NULL;
@@ -1027,22 +1138,31 @@ static PyObject *bora_send_bw_msg( PyObject * self, PyObject * args )
         addr.sin_family = AF_INET;
 
     #if defined(_WIN32) || defined(_WIN64)
-        addr.sin_addr.s_addr = s = inet_addr((const char*)PyString_AsString(PyDict_GetItemString(dict_cur, "ip")));
+        s = win32_inet_aton((const char*)PyString_AsString(PyDict_GetItemString(dict_cur, "ip")), &(addr.sin_addr));
+        if (s == 0) {
+                  printf("return: %i, got: %s", s, PyString_AsString(PyDict_GetItemString(dict_cur, "ip")));
+                  PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  return NULL;
+        }
+
     #else
 
         s = inet_pton(AF_INET, (const char*)PyString_AsString(PyDict_GetItemString(dict_cur, "ip")), &(addr.sin_addr));
-    #endif
-
         if (s <= 0) {
                if (s == 0) {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, got: %s", s, PyString_AsString(PyDict_GetItemString(dict_cur, "ip")));
                   return NULL;
                } else {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, got: %s", s, PyString_AsString(PyDict_GetItemString(dict_cur, "ip")));
                   perror("inet_pton");
                   return NULL;
                }
         }
+
+    #endif
+
 
         if (!PyInt_Check(PyDict_GetItemString(dict_cur, "port"))) {
                 PyErr_SetString(PyExc_AttributeError, "PORT is not INT");
@@ -1161,25 +1281,32 @@ static PyObject *bora_send_cookie( PyObject * self, PyObject * args )
     destaddr1.sin_family = AF_INET;
     destaddr2.sin_family = AF_INET;
 
-    #if defined(_WIN32) || defined(_WIN64)
-    destaddr1.sin_addr.s_addr = s = inet_addr((const char*)dest1);
-    destaddr2.sin_addr.s_addr = s = inet_addr((const char*)dest2);
-    #else
+#if defined(_WIN32) || defined(_WIN64)
+    s = win32_inet_aton((const char*)dest1, &(destaddr1.sin_addr));
+    t = win32_inet_aton((const char*)dest2, &(destaddr2.sin_addr));
+    if (s == 0 || t == 0) {
+                  PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, %i, got: %s %s", s, t, dest1, dest2);
+                  return NULL;
+    }
+
+#else
     s = inet_pton(AF_INET, (const char*)dest1, &(destaddr1.sin_addr));
     t = inet_pton(AF_INET, (const char*)dest2, &(destaddr2.sin_addr));
-    #endif
-/*
     if (s <= 0 || t <= 0) {
                if (s == 0 || t == 0) {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, %i, got: %s %s", s, t, dest1, dest2);
                   return NULL;
                } else {
                   PyErr_SetString(PyExc_AttributeError, "DEST IP: Not in presentation format");
+                  printf("return: %i, %i, got: %s %s", s, t, dest1, dest2);
                   perror("inet_pton");
                   return NULL;
                }
     }
-*/
+#endif
+
     if (port1_num>65535 || port1_num<1 || port2_num>65535 || port2_num<1) {
                   PyErr_SetString(PyExc_AttributeError, "Port number not allowed");
                   return NULL;
